1517f80b8c5f03ad76ed46698dc6b080c3db6a10
Deanna Vickers <deanna2000@gmail.com>
2018-07-06 11:11:06 -0500

    added comments

--- family_dict.py
+++ family_dict.py
@@ -1,3 +1,4 @@
+# Set up the data in a dictionary
 my_family = {
     'brother1': {
         'name': 'Callen',
@@ -21,9 +22,11 @@ my_family = {
     }
 }
 
+# Create empty set
 family_set = set()
+# Iterate over the dictionary and add new items including name and age to a set
 for family_member, member_values in my_family.items():
     family_set.add(f'{member_values["name"]} is my {family_member} and is {str(member_values["age"])} years old')
 
-
+# Print out the family items
 print("My family!", family_set)
